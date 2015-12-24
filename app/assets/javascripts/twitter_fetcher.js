ac198ca8dcec10b55293532461f037172d34586b
HirenBhalani-BTC <hiren.bhalani@botreeconsulting.com>
2015-12-24 15:26:12 +0530

    remove twitter

--- app/assets/javascripts/twitter_fetcher.js
+++ app/assets/javascripts/twitter_fetcher.js
@@ -35,8 +35,8 @@ function login() {
     } else {
       page.evaluate(function(){
         console.log("==> Submitting login form...");
-        document.querySelector("[id='session[username_or_email]']").value = 'parthbarot';
-        document.querySelector("[id='session[password]']").value = 's0nudada';
+        document.querySelector("[id='session[username_or_email]']").value = '';
+        document.querySelector("[id='session[password]']").value = '';
         document.forms[0].submit();
       });
 
